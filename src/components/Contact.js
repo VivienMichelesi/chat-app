7f8af68fe7a217a594232bcdc50c8dd3e297b860
Vivien Michelesi <vivien.michelesi@hotmail.fr>
2020-10-19 11:08:34 +0200

    React 3 ok

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import './Styles.css';
+import React from "react";
+import "./Styles.css";
 
-const presence = true;
-function Contact() {
+function Contact(props) {
   return (
     <div className="Contact">
-      <img className="avatar" src="https://randomuser.me/api/portraits/women/95.jpg" alt="user_picture" />
-        <div>
-          <article className="name">Louise Genet</article>
-          <div className="status">
-            <div className={presence ? "status-online" : "status-offline"}></div>
-            <div className="status-text">{presence ? "Online" : "Offline"}</div>
+      <img className="avatar" src={props.avatar} alt="user_picture" />
+      <div>
+        <article className="name">{props.name}</article>
+        <div className="status">
+          <div
+            className={props.online ? "status-online" : "status-offline"}
+          ></div>
+          <div className="status-text">
+            {props.online ? "Online" : "Offline"}
           </div>
         </div>
+      </div>
     </div>
   );
 }
-
 
 export default Contact;
