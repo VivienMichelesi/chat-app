2fb26048b09d8f36b3b2d0f24964d24bc04aba6b
Vivien Michelesi <vivien.michelesi@hotmail.fr>
2020-10-20 19:06:05 +0200

    Challenge quete react 5

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,23 +1,35 @@
 import React from "react";
 import "./Styles.css";
 
-function Contact(props) {
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: props.online,
+    };
+  }
+render() {
   return (
     <div className="Contact">
-      <img className="avatar" src={props.avatar} alt="user_picture" />
+      <img className="avatar" src={this.props.avatar} alt="user_picture" />
       <div>
-        <article className="name">{props.name}</article>
+        <article className="name">{this.props.name}</article>
         <div className="status">
           <div
-            className={props.online ? "status-online" : "status-offline"}
+            className={this.state.online ? "status-online" : "status-offline"}
+            onClick={event => {
+              const newStatus = !this.state.online;
+              this.setState({ online: newStatus})
+            }}
           ></div>
           <div className="status-text">
-            {props.online ? "Online" : "Offline"}
+            {this.state.online ? "Online" : "Offline"}
           </div>
         </div>
       </div>
     </div>
   );
+}
 }
 
 export default Contact;
